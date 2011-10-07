3e16fa3818d79d5329c2e6d2a9cbd635156fed7c
Aleksandr Lossenko <aleksandr.lossenko@s-f.com>
2011-10-07 12:48:58 +0200

    fake useragent

--- lib/hacker_news/scraper.rb
+++ lib/hacker_news/scraper.rb
@@ -2,15 +2,18 @@ module HackerNews
   
   class Scraper
     
-    URL = "http://news.ycombinator.com"
+    URL = "news.ycombinator.com"
     
     def self.articles(pages = 5)
       
-      pagination = ""
+      pagination = "/"
       result = []
       
       0.upto(pages - 1) do |page|
-        doc = Nokogiri::HTML(open(URL+pagination))
+        response =  Net::HTTP.start( URL, 80 ) do |http|
+          http.get(pagination, "User-Agent" => "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.872.0 Safari/535.2").body
+        end
+        doc = Nokogiri::HTML(response)
         articles = doc.css("td.title > a:first-child")
         next_page = articles.pop
         pagination = next_page[:href]
